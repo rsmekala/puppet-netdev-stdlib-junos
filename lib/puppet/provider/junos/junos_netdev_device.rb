c7d870d9813d6122b20bdd514a082943adccfb0d
rsmekala <rsmekala@juniper.net>
2019-05-16 15:32:39 +0530

    Removed unnecessary indentation changes introduced by IDE

--- lib/puppet/provider/junos/junos_netdev_device.rb
+++ lib/puppet/provider/junos/junos_netdev_device.rb
@@ -40,14 +40,15 @@ module NetdevJunos
       else
         @ready = true
       end
+
     end
 
-    def edit_config(jcfg_obj, format)
+    def edit_config( jcfg_obj, format )
       if jcfg_obj.is_a?(Resource)
         edits = Netconf::JunosConfig.new(:TOP)
         edits << jcfg_obj
         load_config = edits.doc.root
-        NetdevJunos::Log.debug load_config.to_xml, tags: %i[config changes]
+        NetdevJunos::Log.debug load_config.to_xml, :tags => [:config, :changes]
 
       else
         load_config = jcfg_obj
@@ -58,21 +59,24 @@ module NetdevJunos
       # rescue it here.
 
       begin
+
         @edits_count += 1
-        @netconf.rpc.load_configuration(load_config, action: 'replace', format: format)
+        @netconf.rpc.load_configuration( load_config, :action => 'replace', :format => format )
+
       rescue Netconf::RpcError => e
         # the load_configuration may yeield rpc-errors that are in fact not errors,
         # but merely warnings.  Check for that here.
         if rpc_errs = e.rsp.xpath('//rpc-error')
           # ignore warnings ...
           all_count = rpc_errs.count
-          warn_count = rpc_errs.xpath('error-severity').select { |err| err.text == 'warning' }.count
+          warn_count = rpc_errs.xpath('error-severity').select{|err| err.text == 'warning'}.count
           if all_count - warn_count > 0
             @edits_count -= 1
-            NetdevJunos::Log.err "ERROR: load-configuration\n" + e.rsp.to_xml, tags: %i[config fail]
+            NetdevJunos::Log.err "ERROR: load-configuration\n" + e.rsp.to_xml, :tags => [:config, :fail]
           end
         end
       end # rescue block
+
     end # edit_config
 
     ###
@@ -81,39 +85,42 @@ module NetdevJunos
     ###
 
     def commit
+
       NetdevJunos::Log.debug "Checking for changes to commit for catalog #{@catalog_version}"
 
       if @edits_count > 0
 
-        NetdevJunos::Log.info "Committing #{@edits_count} changes.", tags: %i[config commit]
+        NetdevJunos::Log.info "Committing #{@edits_count} changes.", :tags => [:config, :commit]
 
         begin
           report_show_compare
-          @netconf.rpc.commit_configuration(log: "Puppet agent catalog: #{@catalog_version}")
+          @netconf.rpc.commit_configuration( :log => "Puppet agent catalog: #{@catalog_version}" )
         rescue Netconf::RpcError => e
-          NetdevJunos::Log.err "ERROR: Configuration change\n" + e.rsp.to_xml, tags: %i[config fail]
+          NetdevJunos::Log.err "ERROR: Configuration change\n" + e.rsp.to_xml, :tags => [:config, :fail]
         else
-          NetdevJunos::Log.notice 'OK: COMMIT success!', tags: %i[config success]
+          NetdevJunos::Log.notice "OK: COMMIT success!", :tags => [:config, :success]
         ensure
           @netconf.rpc.unlock_configuration
         end
 
       end # -- committing changes
 
-      NetdevJunos::Log.debug 'Closing NETCONF connection'
+      NetdevJunos::Log.debug "Closing NETCONF connection"
       begin
         @netconf.close
-      rescue StandardError
+      rescue
         # ignore - could be in a prior locked condition, and the call to close
         # currently raises an RPC error.
       end
+
     end
 
     def report_show_compare
-      args = { database: 'candidate', compare: 'rollback', rollback: '0', format: 'text' }
-      compare_rsp = @netconf.rpc.get_configuration(args)
+      args = { :database=>'candidate', :compare=>'rollback', :rollback=>'0', :format=>'text' }
+      compare_rsp = @netconf.rpc.get_configuration( args )
       diff = "\n" + compare_rsp.xpath('//configuration-output').text
-      NetdevJunos::Log.notice(diff, tags: %i[config changes])
+      NetdevJunos::Log.notice( diff, :tags => [:config, :changes] )
     end
+
   end #--class
 end #-- module
